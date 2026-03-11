ece767ce1cd72cefed910297fd37b0d9d4010280
jeddelich@gmail.com <jeddelich@gmail.com>
2026-03-11 15:57:13 -0400

    initialize react vite portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
+function App() {
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
